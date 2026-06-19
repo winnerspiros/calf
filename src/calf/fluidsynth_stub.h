3569ec98dc63c627c5caa4be680b2518129b9b27
winnerspiros <winnerspiros@users.noreply.github.com>
2026-06-19 08:12:56 +0000

    fix: add fluidsynth function declarations to stub header
    - fluidsynth_stub.h now includes both type and function declarations
    - fluidsynth_stubs.cpp provides stub implementations
    - modules_dist.cpp can compile without real fluidsynth library

--- src/calf/fluidsynth_stub.h
+++ src/calf/fluidsynth_stub.h
@@ -1,9 +1,38 @@
 // Stub fluidsynth header for Android builds without fluidsynth
-// Provides opaque type definitions so Calf headers compile
-#ifndef FLUIDSYNTH_H
-#define FLUIDSYNTH_H
+// Provides opaque type definitions and stub function declarations
+#ifndef FLUIDSYNTH_STUB_H
+#define FLUIDSYNTH_STUB_H
 
+#ifdef HAVE_FLUIDSYNTH
+#include <fluidsynth.h>
+#else
+// Opaque type forward declarations
 typedef struct _fluid_synth_t fluid_synth_t;
 typedef struct _fluid_settings_t fluid_settings_t;
 
+// Stub function declarations (defined in fluidsynth_stubs.cpp)
+fluid_synth_t* new_fluid_synth(fluid_settings_t* settings);
+void delete_fluid_synth(fluid_synth_t* synth);
+void fluid_synth_set_gain(fluid_synth_t* synth, float gain);
+int fluid_synth_sfload(fluid_synth_t* synth, const char* filename, int reset_presets);
+void fluid_synth_program_select(fluid_synth_t* synth, int chan, int sfont_id, int bank_num, int preset_num);
+void fluid_synth_pitch_wheel_sens(fluid_synth_t* synth, int chan, int val);
+void fluid_synth_pitch_bend(fluid_synth_t* synth, int chan, int val);
+void fluid_synth_noteon(fluid_synth_t* synth, int chan, int key, int vel);
+void fluid_synth_noteoff(fluid_synth_t* synth, int chan, int key);
+void fluid_synth_set_gen(fluid_synth_t* synth, int chan, int param, float value);
+void fluid_synth_write_float(fluid_synth_t* synth, int len, float* lout, int loff, int lincr, float* rout, int roff, int rincr);
+
+fluid_settings_t* new_fluid_settings();
+void delete_fluid_settings(fluid_settings_t* settings);
+void fluid_settings_setnum(fluid_settings_t* settings, const char* name, double val);
+void fluid_settings_setint(fluid_settings_t* settings, const char* name, int val);
+
+// Stub for GEN_ATTENUATION constant
+#ifndef GEN_ATTENUATION
+#define GEN_ATTENUATION 13
 #endif
+
+#endif // !HAVE_FLUIDSYNTH
+
+#endif // FLUIDSYNTH_STUB_H
--- src/fluidsynth_stubs.cpp
+++ src/fluidsynth_stubs.cpp
@@ -1,29 +1,25 @@
-// Stub fluidsynth functions for Android builds
-// These are weak symbols that satisfy the linker when fluidsynth is not available
+// Stub fluidsynth function implementations for Android builds
+// These satisfy the linker when fluidsynth library is not available
 // The vinyl module will be non-functional without fluidsynth, but other modules work
-#include <cstddef>
+#include "calf/fluidsynth_stub.h"
 
-extern "C" {
+#ifndef HAVE_FLUIDSYNTH
 
-struct _fluid_synth_t {};
-struct _fluid_settings_t {};
+fluid_synth_t* new_fluid_synth(fluid_settings_t* settings) { (void)settings; return nullptr; }
+void delete_fluid_synth(fluid_synth_t* synth) { (void)synth; }
+void fluid_synth_set_gain(fluid_synth_t* synth, float gain) { (void)synth; (void)gain; }
+int fluid_synth_sfload(fluid_synth_t* synth, const char* filename, int reset_presets) { (void)synth; (void)filename; (void)reset_presets; return -1; }
+void fluid_synth_program_select(fluid_synth_t* synth, int chan, int sfont_id, int bank_num, int preset_num) { (void)synth; (void)chan; (void)sfont_id; (void)bank_num; (void)preset_num; }
+void fluid_synth_pitch_wheel_sens(fluid_synth_t* synth, int chan, int val) { (void)synth; (void)chan; (void)val; }
+void fluid_synth_pitch_bend(fluid_synth_t* synth, int chan, int val) { (void)synth; (void)chan; (void)val; }
+void fluid_synth_noteon(fluid_synth_t* synth, int chan, int key, int vel) { (void)synth; (void)chan; (void)key; (void)vel; }
+void fluid_synth_noteoff(fluid_synth_t* synth, int chan, int key) { (void)synth; (void)chan; (void)key; }
+void fluid_synth_set_gen(fluid_synth_t* synth, int chan, int param, float value) { (void)synth; (void)chan; (void)param; (void)value; }
+void fluid_synth_write_float(fluid_synth_t* synth, int len, float* lout, int loff, int lincr, float* rout, int roff, int rincr) { (void)synth; (void)len; (void)lout; (void)loff; (void)lincr; (void)rout; (void)roff; (void)rincr; }
 
-_fluid_synth_t* new_fluid_synth(_fluid_settings_t* settings) { return nullptr; }
-void delete_fluid_synth(_fluid_synth_t* synth) {}
-void fluid_synth_set_gain(_fluid_synth_t* synth, float gain) {}
-int fluid_synth_sfload(_fluid_synth_t* synth, const char* filename, int reset_presets) { return -1; }
-void fluid_synth_program_select(_fluid_synth_t* synth, int chan, int sfont_id, int bank_num, int preset_num) {}
-void fluid_synth_pitch_wheel_sens(_fluid_synth_t* synth, int chan, int val) {}
-void fluid_synth_pitch_bend(_fluid_synth_t* synth, int chan, int val) {}
-void fluid_synth_noteon(_fluid_synth_t* synth, int chan, int key, int vel) {}
-void fluid_synth_noteoff(_fluid_synth_t* synth, int chan, int key) {}
-void fluid_synth_set_gen(_fluid_synth_t* synth, int chan, int param, float value) {}
-void fluid_synth_write_float(_fluid_synth_t* synth, int len, float* lout, int loff, int lincr, float* rout, int roff, int rincr) {}
-void fluid_synth_set_sample_rate(_fluid_synth_t* synth, float sample_rate) {}
+fluid_settings_t* new_fluid_settings() { return nullptr; }
+void delete_fluid_settings(fluid_settings_t* settings) { (void)settings; }
+void fluid_settings_setnum(fluid_settings_t* settings, const char* name, double val) { (void)settings; (void)name; (void)val; }
+void fluid_settings_setint(fluid_settings_t* settings, const char* name, int val) { (void)settings; (void)name; (void)val; }
 
-_fluid_settings_t* new_fluid_settings() { return nullptr; }
-void delete_fluid_settings(_fluid_settings_t* settings) {}
-void fluid_settings_setnum(_fluid_settings_t* settings, const char* name, double val) {}
-void fluid_settings_setint(_fluid_settings_t* settings, const char* name, int val) {}
-
-} // extern "C"
+#endif
